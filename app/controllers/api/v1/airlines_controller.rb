deb4460d05a238f9f1ea0c0d9107ce495ee5829d
Vinicius Cerqueira Bonifacio <vinicius.bonifacio1985@gmail.com>
2020-12-17 18:44:43 +0200

    Improves the airlines_controllers.rb to use the before_action filter, applying this way the 'Don't Repeat Yourself' concept

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -1,46 +1,46 @@
 module Api
 	module V1
 		class AirlinesController < ApplicationController
+			before_action :set_entry, only: [:show, :edit, :update, :destroy]
 			protect_from_forgery with: :null_session
-			def index 
-				airlines = Airline.all
 
-				render json: AirlineSerializer.new(airlines, options).serialized_json
+			def index 
+				@airlines = Airline.all
+				render json: AirlineSerializer.new(@airlines, options).serialized_json
 			end
 
 			def show
-				airline = Airline.find_by(slug: params[:slug])
-
-				render json: AirlineSerializer.new(airline, options).serialized_json
+				# the line below nos is made on the before_action
+				# airline = Airline.find_by(slug: params[:slug])
+				render json: AirlineSerializer.new(@airline, options).serialized_json
 			end
 
 			def create
-				airline = Airline.new(airline_params)
-
-				if airline.save
-					render json: AirlineSerializer.new(airline).serialized_json
+				@airline = Airline.new(airline_params)
+				if @airline.save
+					render json: AirlineSerializer.new(@airline).serialized_json
 				else
-					render json: {error: airline.errors.messages}, status: 422
+					render json: {error: @airline.errors.messages}, status: 422
 				end
 			end
 
 			def update
-				airline = Airline.find_by(slug: params[:slug])
-
-				if airline.update(airline_params)
-					render json: AirlineSerializer.new(airline, options).serialized_json
+				# the line below nos is made on the before_action
+				# airline = Airline.find_by(slug: params[:slug])
+				if @airline.update(airline_params)
+					render json: AirlineSerializer.new(@airline, options).serialized_json
 				else
-					render json: {error: airline.errors.messages}, status: 422
+					render json: {error: @airline.errors.messages}, status: 422
 				end			
 			end
 
 			def destroy
-				airline = Airline.find_by(slug: params[:slug])
-
-				if airline.destroy
+				# the line below nos is made on the before_action
+				# airline = Airline.find_by(slug: params[:slug])
+				if @airline.destroy
 					head :no_content
 				else
-					render json: {error: airline.errors.messages}, status: 422
+					render json: {error: @airline.errors.messages}, status: 422
 				end
 			end
 
@@ -52,6 +52,13 @@ module Api
 			def options
 				# @options is an instance variable and is available to all methods within the class.
 				@options ||= { include: %i[reviews]} 
+			end
+
+			# DRY concepts
+			# Combinated with the before_action performs this search for all the controllers
+			# included in the only: [:show, :edit, :update, :destroy] options
+			def set_entry
+				@airline = Airline.find_by(slug: params[:slug])
 			end
 		
 		end
